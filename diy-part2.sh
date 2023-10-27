4cc5085524fe6c5f0fd58e118aa7d0f732f9c71e
xiaoyu20202020 <143551156+xiaoyu20202020@users.noreply.github.com>
2023-10-28 03:16:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,6 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.137.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 #设置密码为空
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
