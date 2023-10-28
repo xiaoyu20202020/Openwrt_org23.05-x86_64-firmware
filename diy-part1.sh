ba279702dca649311dbb87148c5a7bacd0972057
xiaoyu20202020 <143551156+xiaoyu20202020@users.noreply.github.com>
2023-10-28 19:01:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,9 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo 'src-git smallpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/lisaac/luci-app-diskman.git
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
 
 #修改root密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
